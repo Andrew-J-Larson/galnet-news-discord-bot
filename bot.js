156937bc75a1331592a806ddf22a61da77bd4b6a
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2021-01-06 02:03:12 -0800

    Optimized/Simplified code related to dates
    Using MomentJS, I was able to get rid of functions that I made, and simplify the code.

--- bot.js
+++ bot.js
@@ -53,6 +53,7 @@ const GNN_ARCHIVE_URL_PREFIX = ED_COMMUNITY_URL + 'galnet/uid/';
 const GNN_RSS_URL = ED_BACKEND_URL + 'galnet.rss';
 const GNN_JSON_URL = ED_BACKEND_URL + 'api/galnet' + IN_JSON_FORMAT;
 const WINGS_LOGO_ORANGE = ED_COMMUNITY_URL + 'sites/EDSITE_COMM/themes/bootstrap/bootstrap_community/css/images/WingsLogo_Orange.png';
+const REAL_TO_GAME_YEAR_DIFF = 1286;
 // FIX ME!!!
 const FIRST_POST_DATE = '22-08-3304'; // FIX ME!!! Actually is 23-08-3304 and the actual first date ever was 05-07-3301
 const FEED_INTERVAL_SPEED = 60000; // 1 minute in milliseconds
@@ -169,7 +170,7 @@ function saveSettings() {
 
     // write to file
     fs.writeFile(SAVE_FILE, saveString, function (err) {
-        if (err) return console.log(err);
+        if (err) return console.error(err);
         console.log('Settings saved successfully');
     });
 }
@@ -209,70 +210,14 @@ function setPrefix(msg, prefix) {
 
 // BOT FUNCTIONS
 
-// checks if date string is in correct format DD-MM-YYYY
-function validNumberDateString(date) {
-    if (!(date && typeof(date) === 'string')) return false;
-    else {
-        let pattern = /^([0-9]{2})\-([0-9]{2})\-([0-9]{4})$/;
-        if (pattern.test(date)) return true;
-        else return false;
-    }
-}
-
-// checks if date string is in correct format DD-MON-YYYY
-function validMonthDateString(date) {
-    if (!(date && typeof(date) === 'string')) return false;
-    else {
-        let pattern = /^([0-9]{2})\-([A-Z]{3})\-([0-9]{4})$/;
-        if (pattern.test(date)) return true;
-        else return false;
-    }
-}
-
-// get real date from game date, returns date object (in UTC)
-function getRealDate(gameDate) {
-    const DATE_DIFFERENCE = 1286;
-
-    gameDate = gameDate.split('-');
-
-    let realYear = parseInt(gameDate[2], 10) - DATE_DIFFERENCE;
-    
-    let newDate = [parseInt(gameDate[1], 10), parseInt(gameDate[0], 10), realYear].join('/');
-
-    return newDate + ' UTC';
-}
-
-// convert "DD-MM-YYYY" date format to "DD MON YYYY" as used in game
-function articleDateFormat(date) {
-    const MONTH_NAMES = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
-
-    date = date.split('-');
-
-    let newDate = [date[0], MONTH_NAMES[parseInt(date[1]) - 1], date[2]];
-
-    return newDate.join(' ');
-}
-
-// convert "DD MON YYYY" game date format to "DD-MM-YYYY" as used in command
-function commandDateFormat(date) {
-    const MONTH_NAMES = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
-    const zeroPad = (num, places) => String(num).padStart(places, '0');
-
-    date = date.split(' ');
-
-    let newDate = [date[0], zeroPad(MONTH_NAMES.indexOf(date[1]) + 1, 2), date[2]];
-
-    return newDate.join('-');
-}
-
 // sends and formats article post to discord, or to the feed channel if msg is null
-function createArticlePost(msg, post, gameDate) {
+function createArticlePost(msg, post) {
     // parse the HTML for the description
     const PAGE_BREAK = '<br />';
-    let body = escapeMarkdown(post.body.replace(/<p>|<\/p>|\r?\n|\r/g,''));
+    let body = post.body.replace(/<p>|<\/p>|\r?\n|\r/g,'');
     let firstPageBreak = body.indexOf(PAGE_BREAK);
-    let descSentence1st = htmlToText(body.substring(0, firstPageBreak), {wordwrap: null});
-    let descSentencesAfter = htmlToText(body.substring(firstPageBreak + PAGE_BREAK.length), {wordwrap: null});
+    let descSentenceFirst = escapeMarkdown(htmlToText(body.substring(0, firstPageBreak), {wordwrap: null}));
+    let descSentencesMore = escapeMarkdown(htmlToText(body.substring(firstPageBreak + PAGE_BREAK.length), {wordwrap: null}));
 
     (async () => {
         // include the archive link (nice purpose for cases where articles have same slug article link)
@@ -283,7 +228,7 @@ function createArticlePost(msg, post, gameDate) {
         let postArchiveURL = GNN_ARCHIVE_URL_PREFIX + postGUID
         
         // need to size differently for posts larger that 2048 characters
-        let description = ('**' + descSentence1st + '**\n' + descSentencesAfter + '\n**[Archived Post](' + postArchiveURL + ')**').replace(/\n/g, '\n\n');
+        let description = ('**' + descSentenceFirst + '**\n' + descSentencesMore + '\n**[Archived Post](' + postArchiveURL + ')**').replace(/\n/g, '\n\n');
         const desc = [];
         if (description.length > DESCRIPTION_LENGTH) {
             let newDescription = description.substring(0, DESCRIPTION_LENGTH);
@@ -302,14 +247,14 @@ function createArticlePost(msg, post, gameDate) {
                 desc.push(newFieldValue);
             }
             
-        } else if (descSentence1st.length != '') desc.push(description);
+        } else if (descSentenceFirst.length != '') desc.push(description);
 
         const embed = new Discord.MessageEmbed()
           .setColor(MAIN_BOT_COLOR)
           .setAuthor(post.date)
           .setTitle('__' + escapeMarkdown(post.title) + '__')
           .setURL(GNN_ARTICLE_URL_PREFIX + post.slug)
-          .setFooter(getRealDate(gameDate));
+          .setFooter(moment(post.date, 'DD MMM YYYY').subtract(REAL_TO_GAME_YEAR_DIFF, 'y').format('LL') + ' UTC');
 
         // conditionally set image if there is one
         if (post.image && post.image.indexOf(',') != 0) {
@@ -336,7 +281,7 @@ function createArticlePost(msg, post, gameDate) {
     })();
 }
 
-// gets only the very most recent singular post from galnet news; gameDate forces it to grab article from specific date
+// gets only the very most recent singular post from Galnet News
 function getGnnTopPost(msg) {
     (async () => {
         let allNewsJSON = await fetch(GNN_JSON_URL);
@@ -344,46 +289,34 @@ function getGnnTopPost(msg) {
 
         let post = allNews[0];
 
-        // get date from the article
-        let gameDate = commandDateFormat(post.date);
-
         // post new article to channel or feed channel if it was a feed
-        createArticlePost(msg, post, gameDate);
+        createArticlePost(msg, post);
     })();
 }
 
 // gets posts from galnet news; gameDate forces it to grab the article(s) from a specific date
 // if nothing is entered, it'll grab the newest post(s),
-// [for feed] but if a pub date is entered, it'll skip everything including that node, to show newer posts
+// [for feed] but if a postNode is entered, it'll show new old to new posts starting from and skipping postNode
 function getGnnPosts(msg, gameDate, postNode) {
     const ALL_POST_DELAY = 1500;
 
-    let validNumberDate = false;
-    let validMonthDate = false;
-    if (gameDate) {
-        // needed incase it contains a month
-        gameDate = gameDate.toUpperCase();
+    let valid_DD_MM_YYYY = moment(gameDate, 'DD-MM-YYYY'),
+        valid_DD_MMM_YYYY = moment(gameDate, 'DD-MMM-YYYY');
 
-        validNumberDate = validNumberDateString(gameDate);
-        validMonthDate = validMonthDateString(gameDate);
-        if (!validNumberDate && !validMonthDate) {
-            msg.channel.send('Invalid date entered, please put date in format DD-MM-YYYY.');
-            return;
-        }
+    let gameDateMoment = valid_DD_MM_YYYY || valid_DD_MMM_YYYY;
+    if (!gameDateMoment) {
+        msg.channel.send('Invalid date entered, please put date in the correct format.');
+        return;
     }
 
     (async () => {
         let allNewsJSON = await fetch(GNN_JSON_URL);
         let allNews = await allNewsJSON.json();
-
+        // as long as we don't hage a postNode, we need a date to check against
+        let checkDate = !postNode ? (gameDateMoment ? gameDateMoment.format('DD MMM YYYY').toUpperCase()
+                                                    : moment(allNews[0].date, 'DD MMM YYYY'))
+                                  : null;
         let post = null;
-        let checkDate = null;
-
-        // get requested article from date (depending on date type)
-        if (gameDate && validMonthDate) gameDate = commandDateFormat(gameDate.replace(/-/g, ' '));
-        else if (!gameDate) gameDate = commandDateFormat(allNews[0].date);
-        // gameDate needs to convert to format used in posts, when not coming from feed
-        if (!postNode) checkDate = articleDateFormat(gameDate);
 
         // loop through all articles to find oldest with matching date
         const TOTAL_ARTICLES = allNews.length;
@@ -394,7 +327,7 @@ function getGnnPosts(msg, gameDate, postNode) {
             post = allNews[i];
             let peekPost = i < (TOTAL_ARTICLES - 1) ? allNews[i + 1] : null;
 
-            // if we have a gameDate, else a postNode
+            // if we have a date to check, else find postNode
             let currDate, peekDate;
             if (checkDate) {
                 currDate = post.date == checkDate;
@@ -420,16 +353,14 @@ function getGnnPosts(msg, gameDate, postNode) {
             if (checkDate) {
                 // loop and send all articles that matched the date
                 for (let k = i; k >= j; k--) {
-                    setTimeout(function() {createArticlePost(msg, allNews[k], gameDate);}, ALL_POST_DELAY * postIndex);
+                    setTimeout(function() {createArticlePost(msg, allNews[k])}, ALL_POST_DELAY * postIndex);
 
                     postIndex++;
                 }
             } else if (postNode) {
                 // loop and send all articles after the matched date
                 for (let k = j; k >= 0; k--) {
-                    // need this temp date in case we blend into new dates from feed check
-                    let tempDate = commandDateFormat(allNews[k].date);
-                    setTimeout(function() {createArticlePost(msg, allNews[k], tempDate);}, ALL_POST_DELAY * postIndex);
+                    setTimeout(function() {createArticlePost(msg, allNews[k])}, ALL_POST_DELAY * postIndex);
 
                     postIndex++;
                 }
@@ -456,8 +387,7 @@ function getAllGnnPosts(msg) {
         let seconds = 0;
         for (let i = TOTAL_ARTICLES - 1; i >= 0; i--) {
             let post = allNews[i];
-            let gameDate = commandDateFormat(post.date);
-            setTimeout(function() {createArticlePost(msg, post, gameDate);}, ALL_POST_DELAY * (seconds + 1));
+            setTimeout(function() {createArticlePost(msg, post)}, ALL_POST_DELAY * (seconds + 1));
 
             seconds++;
         }
@@ -533,7 +463,7 @@ function checkUpdate() {
 
                 // write to file
                 fs.writeFile(NEWEST_POST_FILE, checkPostNode + '\n', function (err) {
-                    if (err) return console.log(err);
+                    if (err) return console.error(err);
                 }); 
 
                 // get post(s) after the last known post (via node compare) from json file
@@ -597,7 +527,7 @@ client.on('message', msg => {
               .setDescription(`To run a command: \`${settings.prefix} <command>\`\n\n` +
                 `**help** - Brings up this help page\n` +
                 `**ping** - Gets the ping time in milliseconds\n` +
-                `**date [timeline date]** - Gets post(s) from a certain day, but the date format must either be in \`DD-MM-YYYY\` or \`DD-MON-YYYY\`\n` +
+                `**date [timeline date]** - Gets post(s) from a certain day, but the date format must either be in \`DD-MM-YYYY\` or \`DD-MMM-YYYY\`\n` +
                 `**newest** - Gets the latest post(s)\n` +
                 `**top** - Works like newest, but only grabs the single most recent news post\n` +
                 `**feedinfo** - Shows if the feed is on, and what channel it's set to`)
