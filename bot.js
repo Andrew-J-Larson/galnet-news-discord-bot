b44db6b693a8ae6b2269c58af8e06c72443d7406
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2021-01-07 12:59:02 -0800

    Fixed typo

--- bot.js
+++ bot.js
@@ -182,7 +182,7 @@ function saveSettings() {
     for (let i = 0; i < keys.length; i++) {
         let key = keys[i];
         let keyStart = key + '=';
-        if (settings[key]) saveSettings += keyStart + settings[key] + '\n';
+        if (settings[key]) saveString += keyStart + settings[key] + '\n';
     }
 
     // write to file
